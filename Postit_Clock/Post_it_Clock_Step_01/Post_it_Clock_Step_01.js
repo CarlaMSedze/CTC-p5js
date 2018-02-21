834858b35c167fcd5823117793638c4535088ff8
CarlaMSedze <carmuose@gmail.com>
2018-02-21 17:44:14 +0100

    add Post_it_Clock

---
+++ Postit_Clock/Post_it_Clock_Step_01/Post_it_Clock_Step_01.js
@@ -0,0 +1,15 @@
+
+var im;
+function preload() {
+  im = loadImage('https://raw.githubusercontent.com/arduino/CTC-Processing/master/en/_02_PostIt_Clock/Post_it_Clock_Step_01/data/photo.jpg');
+}
+function setup() {
+  createCanvas(400, 400);
+  background(220);  // We make the sketch the size of the image
+  //im = loadImage('photo.jpg');  // Image name
+   image(im, 0, 0);  
+}
+
+function draw() {
+ // To show the image on the screen at the coordinates 0, 0
+}
--- Red_Snake/Red_Snake_Step_06/Red_Snake_Step_06.js
+++ Red_Snake/Red_Snake_Step_06/Red_Snake_Step_06.js
@@ -1,5 +1,6 @@
 var red;
 var time;
+var diameter;
 
 function setup() { 
   createCanvas(400, 400);
@@ -11,12 +12,13 @@ function setup() {
 function draw() { 
 
   time = time + 1;  // Increase time in one unit
-  red = 128 * (1 + sin(time * 2 * PI / frameRate()/2));
+  red = 128 * (1 + sin(time * 2 * PI / frameRate()/ 20));
+  diameter = 50 * (1 + sin( time * 2 * PI / frameRate()/ 5));
   console.log(frameRate());  // Change the color red, repeat every 20s
 
   noStroke();                // Draw borderless shapes
   fill(red, 0, 0);           // Fill shapes with pure red color
-  ellipse(mouseX, mouseY, 30, 30); // Circle's placement following the mouse pointer
+  ellipse(mouseX, mouseY, diameter, diameter); // Circle's placement following the mouse pointer
 
 
 }
