a50432ce09567e651889d05265a8ad6d5f773a3f
CarlaMSedze <carmuose@gmail.com>
2018-02-22 14:25:57 +0100

    step3

--- Postit_Clock/Post_it_Clock_Step_02/Post_it_Clock_Step_02.js
+++ Postit_Clock/Post_it_Clock_Step_02/Post_it_Clock_Step_02.js
@@ -1,15 +1,18 @@
-
 var im;
-//function preload() {
-  //im = loadImage('https://raw.githubusercontent.com/CarlaMSedze/CTC-p5js/master/Postit_Clock/Post_it_Clock_Step_01/photo.jpg');
+var im2;
+
+function preload() {
+  im = loadImage('https://raw.githubusercontent.com/CarlaMSedze/CTC-p5js/master/Postit_Clock/Post_it_Clock_Step_02/photo.jpg');
+  im2 = loadImage('https://raw.githubusercontent.com/CarlaMSedze/CTC-p5js/master/Postit_Clock/Post_it_Clock_Step_02/photo2.jpg');
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
   background(220);  // We make the sketch the size of the image
-  image(im, 0, 0);  
+  
 }
 
 function draw() {
- // To show the image on the screen at the coordinates 0, 0
+  image(im, 0, 0);  // To show the image on the screen at the coordinates 0, 0
+  image(im2, 400, 0);  // To show the image on the screen at the coordinates 0, 0
 }
