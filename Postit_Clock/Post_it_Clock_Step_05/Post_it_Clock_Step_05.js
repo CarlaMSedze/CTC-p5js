f4dc572588aef1553b84bd44f74f6a4dada98ba4
CarlaMSedze <carmuose@gmail.com>
2018-02-28 18:22:27 +0100

    step5

--- Postit_Clock/Post_it_Clock_Step_05/Post_it_Clock_Step_05.js
+++ Postit_Clock/Post_it_Clock_Step_05/Post_it_Clock_Step_05.js
@@ -1,5 +1,5 @@
 var im = [];
-var imFile = ["https://raw.githubusercontent.com/CarlaMSedze/CTC-p5js/master/Postit_Clock/Post_it_Clock_Step_04/photo.jpg","https://raw.githubusercontent.com/CarlaMSedze/CTC-p5js/master/Postit_Clock/Post_it_Clock_Step_04/photo2.jpg"]; // Declare an array to contain 2 images
+var imFile = ["https://github.com/CarlaMSedze/CTC-p5js/blob/master/Postit_Clock/Post_it_Clock_Step_05/one.jpg","https://github.com/CarlaMSedze/CTC-p5js/blob/master/Postit_Clock/Post_it_Clock_Step_05/two.jpg","https://github.com/CarlaMSedze/CTC-p5js/blob/master/Postit_Clock/Post_it_Clock_Step_05/three.jpg","https://github.com/CarlaMSedze/CTC-p5js/blob/master/Postit_Clock/Post_it_Clock_Step_05/four.jpg"]; // Declare an array to contain 2 images
 
 function preload() {
 
@@ -10,15 +10,21 @@ im[1] = loadImage(imFile[1]);
 
 function setup() {
 	
-  createCanvas(800, 400);
+  createCanvas(800, 800);
   background(220);  // We make the sketch the size of the image
+  size(800, 800);  // Make your sketch bigger to show all the images at once   
+  for (int i = 0; i < 4; i = i + 1) {
+    im[i] = loadImage(imFile[i]);  // Load images according to the counter 'i'
+  }
   
 }
 
 function draw() {
 
-  image(im[0], 0, 0); 
+  image(im[0], 0, 0);   // Show the images
   image(im[1], 400, 0);  
+  image(im[2], 0, 400);  
+  image(im[3], 400, 400);  
 
 }
 
